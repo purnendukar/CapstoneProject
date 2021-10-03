3095da49a52feb72e2df464b48c5cdb1313a5581
Purnendu Kar <purnendu.k@mantralabsglobal.com>
2021-10-04 00:34:07 +0530

    chore: kafka consumer storing data in mysql

--- kafka_news_consumer.py
+++ kafka_news_consumer.py
@@ -1,5 +1,22 @@
 from kafka import KafkaConsumer
+from sqlalchemy.orm import sessionmaker
+import json
+
+from .models import News, engine
+
+Session = sessionmaker(bind = engine)
+session = Session()
+
 consumer = KafkaConsumer('news')
-for msg in consumer:
-    print (msg)
+for data in consumer:
+    news = json.loads(data)
+    new_obj = News(
+        title=news.get("title"),
+        date_time=news.get("published_date"),
+        summary=news.get("summary"),
+        topic=news.get("topic"),
+        source=news.get("clean_url")
+    )
+    session.add(new_obj)
+    session.commit()
 
