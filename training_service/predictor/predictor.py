6ade1cfb82603c1628b28abfc9f076f7ec803d65
Purnendu Kar <purnendu.k@mantralabsglobal.com>
2021-10-23 15:55:43 +0530

    feat: training service

---
+++ training_service/predictor/predictor.py
@@ -0,0 +1,97 @@
+from flask import Flask, request, render_template, jsonify
+from numpy.core.numeric import True_
+import pandas as pd
+#from kafka import KafkaProducer
+import json
+from threading import Thread
+from werkzeug.utils import redirect
+from random import randint
+# from pyhive import hive
+from pyspark.sql import SparkSession
+
+import requests
+from bs4 import BeautifulSoup
+
+from utils import text_preprocessor,predictor,preprocess_rdd
+from kafka import KafkaProducer
+
+producer = KafkaProducer(bootstrap_servers='kafka:9092')
+
+import os
+path = os.path.dirname(os.path.abspath(__file__))
+
+app = Flask(__name__, template_folder="templates")
+
+"""
+producer = KafkaProducer(
+    bootstrap_servers = app.config["KAFKA_SERVER"],
+    value_serializer = lambda v: json.dumps(v).encode('utf-8')
+)
+"""
+@app.route("/")
+def home():
+    if request.method == "GET":
+        return render_template(
+            "index.html",
+        )
+
+"""
+def df_send_kafka(df, test_id=None):
+    print('send thread started')
+    for data in json.loads(df.to_json(orient="records")):
+        if test_id:
+            data["test_id"] = test_id
+        producer.send(topic=app.config["TOPIC"], value=data)
+"""
+
+
+@app.route("/news_classify", methods=["POST"])
+def news_classfier():
+    request_data = dict(request.form)
+    print("request_data",request_data)
+
+    url = request_data.get('url')
+        
+    if request.method == "POST":
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        title = soup.find('head').find_all('title')[0].contents[0]
+        description = ""
+        for tag in soup.find_all("meta"):
+            if tag.get("name", None) == "description":
+                description = tag.get("content", "")
+                break
+        data = {
+            "title": title,
+            "summary": description,
+            "source": url
+        }
+        producer.send('news', json.dumps(data).encode())
+        trans_text=text_preprocessor(data["summary"])
+        prediction=predictor(trans_text)
+    return render_template(
+        "index.html",
+        category=prediction
+    )
+
+@app.route("/retrain", methods=["GET", "POST"])
+def test_model():
+    if request.method == "GET":
+        return render_template(
+            "retrain.html",
+        )
+    elif request.method == "POST":
+        test_set = pd.read_csv(request.files["file"],sep='|')
+        sparksession=SparkSession.builder.appName('preprocessor').getOrCreate()
+        rdd=sparksession.createDataFrame(test_set)
+
+        #thread = Thread(target=df_send_kafka, args=(test_set,test_id))
+        #thread.start()
+        preprocess_rdd(rdd)
+
+        return render_template(
+            "retrain.html",result='retraining is complete'
+        )
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1',port=5000,debug=True)
---
+++ training_service/predictor/utils.py
@@ -0,0 +1,63 @@
+from nltk.stem import PorterStemmer
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
+
+import pickle
+import re
+
+
+def text_preprocessor(text):
+    stemmer=PorterStemmer()
+    sents=' '.join([sent for sent in sent_tokenize(text)])
+    words=[word for word in word_tokenize(sents) if not word in stopwords.words('english')]
+    words=[word for word in words if len(word)>1]
+    #words=[word for word in words if not word in punctuation]
+    words=[re.sub('[^a-zA-Z]+', '',word) for word in words]
+    #tagged_words=[nltk.pos_tag(word) for word in words]
+    words=' '.join([stemmer.stem(word) for word in words])
+    vectroizer=pickle.load(open('./models/tfidfvectorizer.pkl','rb'))
+    transformed=vectroizer.transform([words])
+    return transformed
+
+
+def predictor(transformed_text):
+    model=pickle.load(open('./models/multinbmodel.pkl','rb'))
+    classmapper=pickle.load(open('./models/classmapper.pkl','rb'))
+    pred=model.predict(transformed_text)[0]
+    #print(pred)
+    classname=classmapper.get(pred)
+    return classname
+
+def columnmapper(classindex):
+    classmapper=pickle.load(open('./models/classmapper.pkl','rb'))
+    return classmapper.get(int(classindex))
+
+def preprocess_rdd(rdd):
+        rdd=rdd.dropna(how='any')
+        rdd=rdd.collect()
+        corpus=[]
+        topic=[]
+        for row in rdd[:]:
+            text=row['title']+' '+row['summary']
+            #text=row['text']
+            corpus.append(text)
+            topic.append(row['topic'])
+        updated_corpus=[]
+        stemmer=PorterStemmer()
+        for row in corpus:
+            sents=' '.join([sent for sent in sent_tokenize(row)])
+            words=[word for word in word_tokenize(sents) if not word in stopwords.words('english')]
+            words=[word for word in words if len(word)>1]
+            #words=[word for word in words if not word in punctuation]
+            words=[re.sub('[^a-zA-Z0-9.]+', '',word) for word in words]
+            #tagged_words=[nltk.pos_tag(word) for word in words]
+            words=' '.join([stemmer.stem(word) for word in words])
+            updated_corpus.append(words)
+        vectorizer=pickle.load(open('./models/tfidfvectorizer.pkl','rb'))
+        model=pickle.load(open('./models/multinbmodel.pkl','rb'))
+        encoder=pickle.load(open('./models/labelencoder.pkl','rb'))
+        trans_x=vectorizer.transform(updated_corpus)
+        trans_y=encoder.transform(topic)
+        model.fit(trans_x,trans_y)
+        pickle.dump(model,open('./models/multinbmodel.pkl','wb'))
+        
